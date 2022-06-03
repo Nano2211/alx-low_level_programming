1e07bdd4bca47e69971638d39eb10dc972dd40d2
Nano2211 <drmsn67@gmail.com>
2022-06-03 11:25:32 -0700

    added 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main-Prints numbers of base 10, from 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar('\n');
+	/*return: 0*/
+	return (0);
+}
