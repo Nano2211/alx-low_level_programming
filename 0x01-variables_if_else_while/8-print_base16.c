f6ce5bcc6b280ab0ee2051b6fa6c28fcbb493549
Nano2211 <drmsn67@gmail.com>
2022-06-03 13:05:17 -0700

    added file 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main- Prints numbers of base 16 which are 0 to 9 and 
+ * the letters a to f
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alp[6] = "abcdef";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	for (i = 0; i <= 6; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	/*return: 0*/
+	return (0);
+}
+
