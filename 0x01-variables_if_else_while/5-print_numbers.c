49577da9112604228e28ca1fcaead768f38fa932
Nano2211 <drmsn67@gmail.com>
2022-06-03 11:14:02 -0700

    added 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-Prints base 10 numbers from 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d \n", i);
+	}
+	/* return: 0*/
+	return (0);
+}
