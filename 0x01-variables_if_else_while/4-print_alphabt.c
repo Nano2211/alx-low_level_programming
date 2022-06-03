678a36ce295d24be890da693d88f937971268dcc
Nano2211 <drmsn67@gmail.com>
2022-06-03 10:06:02 -0700

    fixed error

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- Prints alphabet in lower case,
+ * but removes the letter e and q
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+       	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i =0; i < 26; i++)
+	{
+		if (i != 4 && i !=16)
+		{
+			putchar(alp[i]);
+		}
+	}
+	putchar('\n');
+	/* return: 0*/
+	return (0);
+}
