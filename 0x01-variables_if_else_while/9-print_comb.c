021e4381145f0cb408de31a0c859c1dcfcb78bd9
Nano2211 <drmsn67@gmail.com>
2022-06-03 13:20:28 -0700

    added 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main-Prints every possible combo
+ * of single digit numbers from 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+		if (i !=9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	/*return: 0*/
+	return (0);
+}
