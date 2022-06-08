e0cc2e3648c89781df388f8790481c16e073f1af
Nano2211 <drmsn67@gmail.com>
2022-06-07 21:18:13 -0700

    print lowercase alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints lower case alphabet x10
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 97;
+		for ( c =97; c <= 122; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
 
