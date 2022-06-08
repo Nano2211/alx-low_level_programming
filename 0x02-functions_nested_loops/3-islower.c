137359937e6927bc2f44fafb1bea648074260ccc
Nano2211 <drmsn67@gmail.com>
2022-06-07 21:49:41 -0700

    fixed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,19 @@
-
+#include "main.h"
+/**
+ * _islower- Checks if char is lowercase
+ * return 1 if true and 0 if false
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if ( c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
