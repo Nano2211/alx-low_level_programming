26e9eb6db2e0b304c0aeff3522106e53a039f952
Nano2211 <drmsn67@gmail.com>
2022-06-07 18:48:26 -0700

    added file 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-Prints _putchar followed by a new line 
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alx[] = "ALX";
+	int i = 0;
+
+	while (alx[i] != '\0')
+	{
+		_putchar(alx[i]);
+		i++;
+	}
+	_putchar("\n");
+	/*return:0*/
+	return (0);
+}
