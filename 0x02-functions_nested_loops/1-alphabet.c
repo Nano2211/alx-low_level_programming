6cc032513d873ef5991bde7362576d2074b4ff22
Nano2211 <drmsn67@gmail.com>
2022-06-07 20:57:20 -0700

    print lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * print_alphabet- Prints lowercase alphabet using _putchar only
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c = 97;
+	while (c <= 122)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	/*return:0*/
+	return (0);
+}
