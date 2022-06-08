43fbbb3a725dde9487f057d1bb6f1c93b13087a4
Nano2211 <drmsn67@gmail.com>
2022-06-07 22:53:09 -0700

    solution to tsk 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit- Prints last digit of a number
+ * 
+ * @n: int type
+ *
+ * Return: Always 0
+ */
+int print_last_digit( int n)
+{
+	int digit ;
 
+	if (n < 0)
+	{
+		digit = (-1 * (n % 10));
+		_putchar(n + '0');
+		return (digit);
+	}
+	else
+	{
+		digit = (n % 10);
+		_putchar(n + '0');
+		return (digit);
+	}
+}
