cc9b6b35dcd67d437e66a0ae65741f1b84b30684
Nano2211 <drmsn67@gmail.com>
2022-06-09 18:07:04 -0700

    added to main.h file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,13 @@
 #define _MAIN_H_
 
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_number(void);
+int _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+
 
 #endif
