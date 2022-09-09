99267eca1c649f9aa3992bd3c5326ff6db55318c
Nano2211 <drmnene@gmail.com>
2022-09-09 03:42:21 -0700

    fixed error

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,12 +20,12 @@ int main(void)
 			b = 48;
 			while (b < 58)
 			{
-				if (a != i && a != b && i = b && a < i && i < b)
+				if (a != i && a != b && i == b && a < i && i < b)
 				{
 				putchar(a);
 				putchar(i);
 				putchar(b);
-				if ( i == 56 && a == 55 && b = 57)
+				if ( i == 56 && a == 55 && b == 57)
 				{
 					break;
 				}
