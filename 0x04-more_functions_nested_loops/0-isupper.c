c8bc5e7002852fcfda0334ef2e9df510729b51d8
Nano2211 <drmnene@gmail.com>
2022-09-16 12:54:21 -0700

    fixed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,3 @@
-/**
- * File: 0-isupper.c
- * Auth: Mihlali Nene
- */
-
 #include "main.h"
 
 /**
@@ -12,13 +7,12 @@
  * Return : 1 if uppercase and 0 if not
  */
 int _isupper(int c)
+
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
 }
