36152d8806eaebba2542b7b9109209c08fad4952
Nano2211 <drmnene@gmail.com>
2022-09-16 13:31:42 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,21 @@
  *
  * Return: no return
  */
+
 void more_numbers(void)
 {
-	int n, count;
+	int n1, n2;
 
-	for (count = 0; count <= 9; count++)
+	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (n = 0; n <= 14; n++)
+		for (n2 = 0; n2 <= 14; n2++)
 		{
-			_putchar((n / 10) + '0');
+			if (n2 > 9)
+			{
+				putchar((n2 / 10) + '0');
+			}
+			putchar((n2 % 10) + '0');
 		}
-		_putchar((n % 10) + '0');
+		putchar(10);
 	}
-	_putchar('\n');
 }
