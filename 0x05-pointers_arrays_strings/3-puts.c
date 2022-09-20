82414510475981b896b3c9c8ced87400431fa578
Nano2211 <drmnene@gmail.com>
2022-09-20 07:16:48 -0700

    t3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,21 +3,14 @@
 #include "main.h"
 
 /**
+ *_puts - prints a string
+ * @str: string to print
  *
- *  *_puts - prints a string
- *
- *   * @str: string to print
- *
- *    *
- *
- *     * Description: prints a string
- *
- *      * On success: returns no error
- *
- *       */
+ * Description: prints a string
+ * On success: returns no error
+ */
 
 void _puts(char *str)
-
 {
 
 	int i = 0;
