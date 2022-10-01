de36ca44be9e23da74bf201546c3203ebf357f51
Nano2211 <drmnene@gmail.com>
2022-09-30 17:19:44 -0700

    submission

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower- Checks if char is lowercase
+ * return 1 if true and 0 if false
+ *@c: The character is ASCII code
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha- Prints whether something is an alphabet regardless of whether
+ * its lower or upper cae
+ * if true it will return 1 and if false it will return 0
+ *
+ * @c: The char that will be checked in ASCII code
+ *
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
